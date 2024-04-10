9f560043a282fcd95f9d18e4465e68e75aeee6c6
mattjermain <mjermain04@gmail.com>
2024-04-10 13:48:59 -0500

    While loop
    I completed the while loop to store all the values in an ArrayList.

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -12,8 +12,35 @@ public class StudentDriver {
         File inputFile = new File("input.csv");
         Scanner input = new Scanner(inputFile);
         
+        ArrayList<StudentFees> studentList = new ArrayList<>();
 
+        while(input.hasNext()){
+            String line = input.nextLine();
+            String[] info = line.split(",");
+            String studentName = info[1];
+            int studentID = Integer.parseInt(info[0]);
+            boolean enrolled = Boolean.parseBoolean(info[2]);
+            
+            if(Integer.parseInt(info[0]) > 300){
+                int months = Integer.parseInt(info[3]);
+                studentList.add(new OnlineStudent(studentName, studentID, enrolled, months));
+            }else if(Integer.parseInt(info[0]) > 200){
+                int coursesEnrolled = Integer.parseInt(info[3]);
+                boolean ga = Boolean.parseBoolean(info[4]);
+                String gaType = info[5];
+                studentList.add( new GraduateStudent(studentName, studentID, enrolled, coursesEnrolled, ga, gaType));
+            }else{
+                boolean scholarship = Boolean.parseBoolean(info[4]);
+                double scholarshipAmount = Double.parseDouble(info[5]);
+                int coursesEnrolled = Integer.parseInt(info[3]);
+                studentList.add(new UGStudent(studentName, studentID, enrolled, scholarship, scholarshipAmount, coursesEnrolled));
+            }              
+            
+        }
         
+        for(StudentFees s: studentList){
+            System.out.println(s);
+        }
         
       
     }
